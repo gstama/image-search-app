088071151205443e09dac9a579cac697b99037da
gstama <gstama@outlook.com>
2021-02-15 18:29:20 +0000

    adds ImageCard component for rendering, along with CSS styling to fit images

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
+import './App.css';
 import React from 'react';
 import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 import ImageList from './ImageList';
-import './App.css';
 
 class App extends React.Component {
   state = { images: [] };
